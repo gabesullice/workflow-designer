3e1bd382c9671541b9426f148a26917ffa0b9162
Gabriel Sullice <gabriel@sullice.com>
2025-09-03 12:41:04 +0200

    scaffold: project files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Workflow Designer</h1>
+      <p>Coming Soon</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx}",
+    "./public/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
